a3445e5cc6c3211c48fb9206f2256ad1f682a3e5
sahilyadav-dev <sahilyadav29399@gmail.com>
2025-06-20 14:59:46 +0530

    script.js

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
-emailjs.init("ZTGwUPapNnDhA1naL"); // Replace this!
+emailjs.init("ZTGwUPapNnDhA1naL");
 
-document.querySelector('.contact-form').addEventListener('submit', function (e) {
+document
+  .querySelector(".contact-form")
+  .addEventListener("submit", function (e) {
     e.preventDefault();
 
-    emailjs.sendForm('service_7llmtkz', 'template_3p2ofom', this)
-    .then(function () {
+    emailjs.sendForm("service_7llmtkz", "template_3p2ofom", this).then(
+      function () {
         alert("Message sent successfully!");
-    }, function (error) {
+      },
+      function (error) {
         alert("Failed to send message: " + error.text);
-    });
-});
+      }
+    );
+  });
 
-
-const sidebar= document.querySelector('.responsive-nav')
+const sidebar = document.querySelector(".responsive-nav");
 const closeButton = document.querySelector(".js-close-button");
-const menuButton = document.querySelector(".js-menu-button")
+const menuButton = document.querySelector(".js-menu-button");
 
 function showSidebar() {
-  sidebar.style.display="flex";
-  closeButton.style.display='block'
-  menuButton.style.display='none'
+  sidebar.style.display = "flex";
+  closeButton.style.display = "block";
+  menuButton.style.display = "none";
 }
-function hideSidebar(){
-  sidebar.style.display="none";
-  closeButton.style.display='none'
-  menuButton.style.display='block'
+function hideSidebar() {
+  sidebar.style.display = "none";
+  closeButton.style.display = "none";
+  menuButton.style.display = "block";
 }
 
-sidebar.addEventListener( 'click', () => {
-  hideSidebar()
-})
+sidebar.addEventListener("click", () => {
+  hideSidebar();
+});
